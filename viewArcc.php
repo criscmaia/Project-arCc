ddd6b3d3658bf019ae5e4e69550335ef2b0236b5
Cristiano Maia <criscmaia@gmail.com>
2021-05-11 13:12:47 -0300

    repository + key details fixed

--- viewArcc.php
+++ viewArcc.php
@@ -11,18 +11,49 @@
     <title>Project-arCc</title>
 </head>
 
-<body>
-    <h1>List of approved files: </h1>
-    <?php
-        $files = array_slice(scandir('./repository_approved'), 2);
+<body class="limit1130 bg-light">
+    <nav class="navbar navbar-expand-lg sticky-top" id="menu"></nav>
+    <div class="container-fluid" id="banner"></div>
 
-        sort($files); // this does the sorting
-        echo '<ul>';
-        foreach($files as $file){
-           echo'<li><a href="./repository_approved/'.$file.'">'.$file.'</a></li>';
-        }
-        echo '</ul>';
-    ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-9">
+
+
+                <h1>List of approved files: </h1>
+                <?php
+                    $files = array_slice(scandir('./repository_approved'), 2);
+
+                    sort($files); // this does the sorting
+                    echo '<ul>';
+                    foreach($files as $file){
+                       echo'<li><a href="./repository_approved/'.$file.'">'.$file.'</a></li>';
+                    }
+                    echo '</ul>';
+                ?>
+
+
+
+            </div>
+            <div class="col-lg-3 dark-gray" id="keyDetails"></div>
+        </div>
+    </div>
+
+    <footer class="footer" id="footer"></footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
+<script>
+    $(function() {
+        $("#menu").load("menu.html", function() {
+            var pg = '/Project-arCc' + window.location.href.substring(window.location.href.lastIndexOf("/"));
+            $('a[href="' + pg + '"]').parent('li').addClass('active');
+        });
+        $("#banner").load("banner.html");
+        $("#keyDetails").load("keyDetails.html");
+        $("#footer").load("footer.html");
+    });
+
+</script>
 
 </html>
